39c0ff7c4af8533f16ccbf8833a11e658687c9c8
yuyi <38847066+ys558@users.noreply.github.com>
2019-06-04 20:16:06 +0800

    Create logger.js

---
+++ logger.js
@@ -0,0 +1,14 @@
+const EventEmitter = require('events')
+const uuid = require('uuid')
+
+class Logger extends EventEmitter {
+    log(msg) {
+        // call event
+        this.emit('message', { 
+            id: uuid.v4(), 
+            msg: msg
+        })
+    }
+}
+
+module.exports = Logger;
