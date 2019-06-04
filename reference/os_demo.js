9a0d565b6c0c7b080e96a6b00cc0efe5c9cd63f7
yuyi <38847066+ys558@users.noreply.github.com>
2019-06-04 20:18:34 +0800

    Create os_demo.js

---
+++ reference/os_demo.js
@@ -0,0 +1,22 @@
+const os = require('os')
+
+// platform
+// console.log(os.platform());
+
+// CPU Arch
+// console.log(os.arch());
+
+// CPU Core Info
+// console.log(os.cpus());
+
+// Free memory
+// console.log(os.freemem());
+
+// total memory
+// console.log(os.totalmem());
+
+// home dir
+// console.log(os.homedir());
+
+// uptime
+console.log(os.uptime());
